a5c5e26ad5e55adf79d204b002bcd1a42b095ef4
AbuEl3abbas <marwan20000@live.com>
2021-10-31 21:19:31 +0200

    test

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require('express');
 
+const app = express();
 
+app.get('/', function(req, res)  { 
+    res.send("marwan abbas");
+})
+
+
+app.listen(3000,() => {
+    console.log("app is running");
+})
